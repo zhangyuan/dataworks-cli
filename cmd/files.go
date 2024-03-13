e497f810572a3ec0bc039f5695901b22dbf3ca4d
zhangyuan <zhangyuan@users.noreply.github.com>
2024-03-13 22:28:06 +0800

    download dataworks scripts

---
+++ cmd/files.go
@@ -0,0 +1,106 @@
+package cmd
+
+import (
+	"aliyun-dataworks/pkg/dataworks"
+	"encoding/json"
+	"log"
+	"os"
+
+	"github.com/samber/lo"
+	"github.com/spf13/cobra"
+)
+
+var filesCmd = &cobra.Command{
+	Use: "files",
+}
+
+var listFilesCmd = &cobra.Command{
+	Use: "list",
+	Run: func(cmd *cobra.Command, args []string) {
+		file, err := os.Create(listOutputPath)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer file.Close()
+
+		files, err := dataworks.GetScriptsWithContent()
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		bytes, err := json.MarshalIndent(files, "", "  ")
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		if _, err := file.Write(bytes); err != nil {
+			log.Fatalln(err)
+		}
+	},
+}
+
+var listAllFilesCmd = &cobra.Command{
+	Use: "list-all",
+	Run: func(cmd *cobra.Command, args []string) {
+		file, err := os.Create(listOutputPath)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer file.Close()
+
+		files, err := dataworks.ListFiles()
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		bytes, err := json.MarshalIndent(files, "", "  ")
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		if _, err := file.Write(bytes); err != nil {
+			log.Fatalln(err)
+		}
+	},
+}
+
+var fetchFilesCmd = &cobra.Command{
+	Use: "download",
+	Run: func(cmd *cobra.Command, args []string) {
+		bytes, err := os.ReadFile(filesListFilePath)
+		if err != nil {
+			log.Fatalln(err)
+		}
+		var files []dataworks.NormalFile
+		if err := json.Unmarshal(bytes, &files); err != nil {
+			log.Fatalln(err)
+		}
+
+		lo.ForEach(files, func(file dataworks.NormalFile, _ int) {
+			dataworks.DownloadFile(file, filesOutputDirectoryPath)
+		})
+	},
+}
+
+var listOutputPath string
+
+var filesListFilePath string
+var filesOutputDirectoryPath string
+
+func init() {
+	rootCmd.AddCommand(filesCmd)
+
+	filesCmd.AddCommand(listAllFilesCmd)
+	listAllFilesCmd.Flags().StringVarP(&listOutputPath, "out", "o", "", "puth to file list output")
+	listAllFilesCmd.MarkFlagRequired("out")
+
+	filesCmd.AddCommand(listFilesCmd)
+	listFilesCmd.Flags().StringVarP(&listOutputPath, "out", "o", "", "puth to file list output")
+	listFilesCmd.MarkFlagRequired("out")
+
+	filesCmd.AddCommand(fetchFilesCmd)
+	fetchFilesCmd.Flags().StringVarP(&filesListFilePath, "input", "i", "", "path to file list")
+	fetchFilesCmd.Flags().StringVarP(&filesOutputDirectoryPath, "out", "o", "", "path to files directory")
+	fetchFilesCmd.MarkFlagRequired("input")
+	fetchFilesCmd.MarkFlagRequired("output")
+}
--- pkg/dataworks/dataworks.go
+++ pkg/dataworks/dataworks.go
@@ -1,13 +1,21 @@
 package dataworks
 
 import (
-	"encoding/json"
 	"fmt"
 	"os"
+	"path/filepath"
 	"strconv"
+	"time"
 
 	openapi "github.com/alibabacloud-go/darabonba-openapi/v2/client"
 	dataworks_public20200518 "github.com/alibabacloud-go/dataworks-public-20200518/v6/client"
+
+	"github.com/samber/lo"
+	lop "github.com/samber/lo/parallel"
+)
+
+const (
+	NOMARL_USE_TYPE = "NORMAL"
 )
 
 func CreateClient() (_result *dataworks_public20200518.Client, _err error) {
@@ -19,21 +27,21 @@ func CreateClient() (_result *dataworks_public20200518.Client, _err error) {
 		AccessKeySecret: &accessKeySecret,
 	}
 
-	endpoint := "dataworks.cn-beijing.aliyuncs.com"
+	endpoint := os.Getenv("DATAWORKS_ENDPOINT")
 	config.Endpoint = &endpoint
 	return dataworks_public20200518.NewClient(config)
 }
 
-func GetFiles() error {
+func ListFiles() ([]*dataworks_public20200518.ListFilesResponseBodyDataFiles, error) {
 	client, err := CreateClient()
 	if err != nil {
-		return err
+		return nil, err
 	}
 
-	projectIdString := os.Getenv("PROJECT_ID")
+	projectIdString := os.Getenv("DATAWORKS_PROJECT_ID")
 	projectId, err := strconv.ParseInt(projectIdString, 10, 64)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
 	files := []*dataworks_public20200518.ListFilesResponseBodyDataFiles{}
@@ -47,8 +55,9 @@ func GetFiles() error {
 		}
 		res, err := client.ListFiles(listFilesRequest)
 		if err != nil {
-			return nil
+			return nil, err
 		}
+
 		files = append(files, res.Body.Data.Files...)
 
 		if pageNumber*pageSize >= *res.Body.Data.TotalCount {
@@ -59,15 +68,104 @@ func GetFiles() error {
 
 	}
 
-	bytes, err := json.Marshal(files)
+	return files, nil
+}
+
+type NormalFile struct {
+	FileId         int64
+	FolderId       string
+	Content        string
+	FileName       string
+	FileType       int32
+	LastEditTime   time.Time
+	LastEditUser   string
+	CreateTime     time.Time
+	CreateUser     string
+	ConnectionName string
+}
+
+func GetScriptsWithContent() ([]NormalFile, error) {
+	allFiles, err := ListFiles()
+	if err != nil {
+		return nil, err
+	}
+
+	normalFiles := lo.Filter(allFiles, func(item *dataworks_public20200518.ListFilesResponseBodyDataFiles, index int) bool {
+		return *item.UseType == NOMARL_USE_TYPE && !lo.Contains([]int{23, 99, 1119}, int(*item.FileType))
+	})
+
+	files := lop.Map(normalFiles, func(x *dataworks_public20200518.ListFilesResponseBodyDataFiles, _ int) NormalFile {
+		return NormalFile{
+			FileId:         *x.FileId,
+			FolderId:       *x.FileFolderId,
+			ConnectionName: *x.ConnectionName,
+			FileName:       *x.FileName,
+			FileType:       *x.FileType,
+			LastEditTime:   time.UnixMilli(*x.LastEditTime),
+			LastEditUser:   *x.LastEditUser,
+			CreateUser:     *x.CreateUser,
+			CreateTime:     time.UnixMilli(*x.CreateTime),
+			Content:        *x.Content,
+		}
+	})
+	return files, nil
+}
+
+func GetFileContent(file NormalFile) (string, error) {
+	client, err := CreateClient()
+	if err != nil {
+		return "", err
+	}
+
+	projectIdString := os.Getenv("DATAWORKS_PROJECT_ID")
+	projectId, err := strconv.ParseInt(projectIdString, 10, 64)
+	if err != nil {
+		return "", err
+	}
+
+	getFilesRequest := dataworks_public20200518.GetFileRequest{
+		ProjectId: &projectId,
+		FileId:    &file.FileId,
+	}
+
+	res, err := client.GetFile(&getFilesRequest)
+	if err != nil {
+		return "", err
+	}
+
+	return *res.Body.Data.File.Content, nil
+}
+
+func DownloadFile(file NormalFile, directory string) error {
+	targetFolder := filepath.Join(directory, fmt.Sprintf("%s.%s", file.ConnectionName, file.FolderId))
+	if err := os.MkdirAll(targetFolder, os.ModePerm); err != nil {
+		return err
+	}
+
+	content, err := GetFileContent(file)
 	if err != nil {
 		return err
 	}
 
-	documents := string(bytes)
+	targetFilePath := filepath.Join(targetFolder, file.FileName)
+	fileExt := GetFileExt(file.FileType)
+	if fileExt != "" {
+		targetFilePath = fmt.Sprintf("%s.%s", targetFilePath, fileExt)
+	}
+	targetFile, err := os.Create(targetFilePath)
+	if err != nil {
+		return err
+	}
+	defer targetFile.Close()
 
-	fmt.Println(documents)
+	_, err = targetFile.Write([]byte(content))
+	return err
+}
 
-	return nil
-
+func GetFileExt(fileType int32) string {
+	// ODPS SQL
+	if fileType == 10 {
+		return "sql"
+	}
+	return ""
 }
