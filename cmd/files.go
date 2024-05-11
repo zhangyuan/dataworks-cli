0f2b982e0e81e67fbe3bfb8d82b7bf3a37932748
zhangyuan <zhangyuan@users.noreply.github.com>
2024-05-11 14:07:22 +0800

    list-di-tasks

--- cmd/files.go
+++ cmd/files.go
@@ -65,6 +65,31 @@ var listAllFilesCmd = &cobra.Command{
 	},
 }
 
+var listDITasksCmd = &cobra.Command{
+	Use: "list-di-tasks",
+	Run: func(cmd *cobra.Command, args []string) {
+		file, err := os.Create(diTasksOutputPath)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer file.Close()
+
+		files, err := dataworks.ListDISyncTasks(taskType, dataSourceName)
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		bytes, err := json.MarshalIndent(files, "", "  ")
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		if _, err := file.Write(bytes); err != nil {
+			log.Fatalln(err)
+		}
+	},
+}
+
 var fetchFilesCmd = &cobra.Command{
 	Use: "download",
 	Run: func(cmd *cobra.Command, args []string) {
@@ -91,6 +116,11 @@ var listOutputPath string
 var filesListFilePath string
 var filesOutputDirectoryPath string
 
+var diTasksOutputPath string
+
+var taskType string
+var dataSourceName string
+
 func init() {
 	rootCmd.AddCommand(filesCmd)
 
@@ -102,9 +132,17 @@ func init() {
 	listFilesCmd.Flags().StringVarP(&listOutputPath, "out", "o", "", "puth to file list output")
 	_ = listFilesCmd.MarkFlagRequired("out")
 
+	filesCmd.AddCommand(listDITasksCmd)
+	listDITasksCmd.Flags().StringVarP(&diTasksOutputPath, "out", "o", "", "puth to file list output")
+	_ = listDITasksCmd.MarkFlagRequired("out")
+	listDITasksCmd.Flags().StringVarP(&taskType, "task-type", "t", "DI_OFFLINE", "DI task type")
+	listDITasksCmd.Flags().StringVarP(&dataSourceName, "data-source-name", "s", "", "DI task data source name")
+	_ = listDITasksCmd.MarkFlagRequired("data-source-name")
+
 	filesCmd.AddCommand(fetchFilesCmd)
 	fetchFilesCmd.Flags().StringVarP(&filesListFilePath, "input", "i", "", "path to file list")
 	fetchFilesCmd.Flags().StringVarP(&filesOutputDirectoryPath, "out", "o", "", "path to files directory")
 	_ = fetchFilesCmd.MarkFlagRequired("input")
 	_ = fetchFilesCmd.MarkFlagRequired("output")
+
 }
