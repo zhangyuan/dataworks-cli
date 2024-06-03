05a7d251aca0153453bd0382e89690987fee5196
zhangyuan <zhangyuan@users.noreply.github.com>
2024-06-03 13:33:03 +0800

    sort nodes and tables

--- pkg/dataworks/nodes.go
+++ pkg/dataworks/nodes.go
@@ -1,6 +1,10 @@
 package dataworks
 
 import (
+	"fmt"
+	"sort"
+	"strings"
+
 	dataworks_public20200518 "github.com/alibabacloud-go/dataworks-public-20200518/v6/client"
 )
 
@@ -31,5 +35,29 @@ func (client *Client) ListNodes(projectEnv string) ([]*dataworks_public20200518.
 
 	}
 
+	sortKey := func(node dataworks_public20200518.ListNodesResponseBodyDataNodes) string {
+		keys := []string{}
+
+		if node.BusinessId != nil {
+			keys = append(keys, fmt.Sprintf("%d", *node.BusinessId))
+		} else {
+			keys = append(keys, "a")
+		}
+
+		if node.BaselineId != nil {
+			keys = append(keys, fmt.Sprintf("%d", *node.BaselineId))
+		} else {
+			keys = append(keys, "a")
+		}
+
+		keys = append(keys, fmt.Sprintf("%s", *node.NodeName))
+
+		key := strings.Join(keys, "/")
+		return key
+	}
+
+	sort.Slice(nodes, func(i, j int) bool {
+		return strings.Compare(sortKey(*nodes[i]), sortKey(*nodes[j])) > 0
+	})
 	return nodes, nil
 }
